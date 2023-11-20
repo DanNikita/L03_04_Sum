4b844b5c91ae6cf94c92919d70b55417acbb8ee2
DanilovNV <1995dnv@gmail.com>
2023-11-21 01:25:19 +0300

    Add project files.

---
+++ sum.cs
@@ -0,0 +1,17 @@
+﻿internal class sum
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Number (from 1 to 1000");
+        int number = int.Parse(Console.ReadLine());
+        int result = 0, i = 0;
+        do
+        {
+            result = result + i;
+            Console.WriteLine("{0},{1}", i, result);
+            i++;
+        }
+        while (i <= number);
+    }
+}
+
